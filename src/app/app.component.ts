5ad70a7fac360f78cd300d2d528870786c00de3a
Ali Sheerzad <ali.sheerzad9@gmail.com>
2022-05-20 10:32:58 +0300

    Added form template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent implements OnInit {
   myForm: FormGroup;
 
   ngOnInit(): void {
     this.initForm();
   }
 
-  onSubmit(){}
+  onSubmit(formData: NgForm) {
+    console.log(formData);
+  }
 
   initForm() {
     this.myForm = new FormGroup({
       cryptoCurrency: new FormControl('', [Validators.required]),
       toCurrency: new FormControl('', [Validators.required]),
       fromDate: new FormControl('', [Validators.required]),
-      toDate: new FormControl('', [Validators.required])
-    })
+      toDate: new FormControl('', [Validators.required]),
+    });
   }
 }
